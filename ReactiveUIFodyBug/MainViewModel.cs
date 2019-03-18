12c3f4e144e3f52e8657bb5f22e8b8540d7a171b
Leszek Rogowski <leszek.rogowski@vtssoftware.com>
2019-03-18 17:36:28 +0100

    Add solution which reproduces AmbiguousMatchException in Fody generated code

---
+++ ReactiveUIFodyBug/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace ReactiveUIFodyBug
+{
+    public class MainViewModel : ReactiveObject
+    {
+        public MainViewModel()
+        {
+            this.WhenAnyValue(
+                x => x.FirstName,
+                x => x.LastName,
+                (firstName, lastName) => $"{firstName} {lastName}")
+                .ToPropertyEx(this, x => x.FullName);
+        }
+
+        [Reactive]
+        public string FirstName { get; set; } = "Test 1";
+
+        [Reactive]
+        public string LastName { get; set; } = "Test 2";
+
+        public extern string FullName { [ObservableAsProperty]get; }
+    }
+}
